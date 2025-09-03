94dd90e156d1a96949d363219b6166980b2a6a5b
26harshil <harshil2610@gmail.com>
2025-09-03 23:51:50 +0530

    work

--- data/BrightMindContext.cs
+++ data/BrightMindContext.cs
@@ -1,7 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using BrightMindQuizApi.Models;
 
-//6yg qy6jn6yy7 asd
+//6yg qy6jn6yy7 asd adjnasnn
 namespace BrightMindQuizApi.Data;
 
 public class BrightMindContext : DbContext
