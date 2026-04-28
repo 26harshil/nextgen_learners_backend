216b2ba5de6ab9c5c1f49faf90a56970b048eb9b
26harshil <harshilsolanki2610@gmail.com>
2026-04-28 21:47:36 +0530

    bug-13

--- controller/QuizController.cs
+++ controller/QuizController.cs
@@ -39,7 +39,7 @@ public class QuizController : ControllerBase
     public async Task<ActionResult<IEnumerable<QuestionDto>>> GetMathQuiz()
     {
         return await GetQuizQuestions(
-            fallbackQuizId: ,8
+            fallbackQuizId: 1,
             quizTitleCandidates: ["Math Mastery", "Math for Kids"]
         );
     }
